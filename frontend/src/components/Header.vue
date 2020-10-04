f309ed3d47f26866dfe1f9c7bf9ead7b723c29c6
cymbals <280051755@qq.com>
2020-10-04 20:08:08 +0800

    网站header，网站home路由，关掉eslint

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <img class="logo" src="@/assets/Eu_flag.png" width="70px" />
+    <span class="title">New Phase of EU-China relation for 2020-2030</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style scoped>
+.header {
+  position: absolute;
+  line-height: 50px;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  background-color: #2d3a4b;
+}
+
+.logo {
+  vertical-align: middle;
+  padding: 0px 10px 0px 10px;
+}
+
+.title {
+  position: absolute;
+  color: white;
+  font-weight: bold;
+  font-size: 25px;
+}
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<Header></Header>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  components: {
+    Header,
+  }
+};
+</script>
+
+<style scoped>
+</style>
