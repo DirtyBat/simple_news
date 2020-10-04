fb89b905c2fb27f9163fb7e139dfc917611d2820
280051755@qq.com <280051755@qq.com>
2020-10-04 23:08:21 +0800

    后端图床api

--- backend/news/models.py
+++ backend/news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class NewsData(models.Model):
     # 新闻id
-    id = models.IntegerField(default=-1,primary_key=True)
+    id = models.AutoField(primary_key=True)
     # 新闻标题
     title = models.CharField(max_length=80, default="")
     # 新闻主要内容，可能要存图片的base64，所以开大一点
@@ -14,3 +14,10 @@ class NewsData(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Pics(models.Model):
+    # 图片id
+    id = models.AutoField(primary_key=True)
+    # 图片
+    pic = models.ImageField(upload_to="./pics/%Y-%m-%d")
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,11 +1,13 @@
 # -*-coding:utf-8-*-
 
+
+from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from .models import NewsData
+from .models import NewsData, Pics
 from .serializers import NewsDataSerializer
 
 
@@ -24,6 +26,26 @@ class SubmitNewsView(APIView):
     def post(self, request):
         data = request.data
         # todo
+
+
+class SubmitPicView(APIView):
+    def post(self, request):
+        data = request.data
+        pic = Pics(pic=data.get('pic'))
+        pic.save()
+        return Response({"id":str(pic.id)}, HTTP_200_OK)
+
+
+    def get(self, request):
+        data = request.query_params.dict()
+        id = data.get('id')
+        if not id or not Pics.objects.get(id=id).pic:
+            id = 1
+        
+        pic_path = str(Pics.objects.get(id=id).pic)
+        with open(pic_path, 'rb') as f:
+            pic_data = f.read()
+        return HttpResponse(pic_data, content_type="image/png")
 
 
 # 前端入口
