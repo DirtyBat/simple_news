80a9cad73ed5fa7734278bf8d7064ea6f796b11e
cymbals <280051755@qq.com>
2020-10-04 16:45:46 +0800

    修复main.js报错

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,12 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+import App from './App'
+import router from './router'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
-
-import App from './App'
-import router from './router'
 
 Vue.config.productionTip = false
 Vue.config.debug = true
@@ -16,7 +16,6 @@ Vue.config.debug = true
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = process.env.API_ROOT
 Vue.prototype.$axios = axios
-
 
 /* eslint-disable no-new */
 new Vue({
