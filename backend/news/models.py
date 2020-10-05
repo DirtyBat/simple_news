f4a50729ffee83e778c4bd79cf3434d689d4d7b9
280051755@qq.com <280051755@qq.com>
2020-10-05 21:01:13 +0800

    探索性微调后端新闻api

--- backend/news/models.py
+++ backend/news/models.py
@@ -7,10 +7,16 @@ class NewsData(models.Model):
     id = models.AutoField(primary_key=True)
     # 新闻标题
     title = models.CharField(max_length=80, default="")
-    # 新闻主要内容，可能要存图片的base64，所以开大一点
+    # 新闻主要内容
     text = models.CharField(max_length=300000, default="")
     # 创建日期
     date = models.DateField(auto_now_add=True)
+    # 是否是头条
+    headline = models.BooleanField(default=False)
+    # 简介
+    summary = models.CharField(max_length=2000, default="")
+    # 封面图片,等待设置一个默认连接
+    cover = models.CharField(max_length=50,default="http://47.103.209.239/api/pic/?code=ETfHx14vX3")
 
     def __str__(self):
         return self.title
