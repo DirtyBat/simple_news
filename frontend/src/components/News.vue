9ad1fd34a3eb157c0aa207eb856c1225ef200015
lizonghong <lizonghong@corp.netease.com>
2020-10-06 13:00:02 +0800

    news 分页

--- frontend/src/components/News.vue
+++ frontend/src/components/News.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NewCard v-for="(item, index) in news_data" :key=index
+    <NewCard v-for="(item, index) in current_news_data" :key=index
       :title=item.title
       :date=item.date
       :image_url=item.cover
@@ -9,6 +9,14 @@
       style="margin: 5px;"
     >
     </NewCard>
+    <el-pagination
+      @current-change="handleCurrentChange"
+      small
+      layout="prev, pager, next"
+      :total="news_total"
+      :page-size="page_size"
+    >
+    </el-pagination>
   </div>
 </template>
 <script>
@@ -20,9 +28,27 @@ export default {
     return {
       // api get data
       news_data: null,
+      // page set
+      current_page:0,
+      page_size:3,
     };
   },
-  methods: {},
+  methods: {
+    handleCurrentChange: function(cur_page){
+      // el-pagination start from 1 not 0
+      this.current_page = cur_page -1;
+    }
+  },
+  computed:{
+    current_news_data: function(){
+      if (this.news_data)
+        return this.news_data.slice(this.current_page * this.page_size,
+        (this.current_page + 1) * this.page_size)
+    },
+    news_total: function(){
+        return this.news_data?this.news_data.length:0;
+    }
+  },
   mounted () {
     this.$axios
       .get('api/news_data/')
