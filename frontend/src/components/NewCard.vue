345015cd2177a895a1c879cf5e61678a0267c3f6
XOng-lzh <863670385@qq.com>
2020-10-09 17:06:55 +0800

    編輯和刪除

--- frontend/src/components/NewCard.vue
+++ frontend/src/components/NewCard.vue
@@ -2,26 +2,34 @@
   <div>
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <el-link :underline="false" class="title" @click="ClickToDetail" >{{title}}</el-link>
-        <span class="date"><i class="el-icon-time">{{' '+ date}}</i></span>
+        <el-link :underline="false" class="title" @click="ClickToDetail">{{
+          title
+        }}</el-link>
+        <span class="date"
+          ><i class="el-icon-time">{{ " " + date }}</i></span
+        >
       </div>
       <el-container>
         <el-main>
           <div class="summary">
-            {{summary}}
+            {{ summary }}
           </div>
         </el-main>
         <el-aside>
           <!--<img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">-->
           <a href="">
-            <el-image v-bind:src="image_url" class="image" @click="ClickToDetail"></el-image>
+            <el-image
+              v-bind:src="image_url"
+              class="image"
+              @click="ClickToDetail"
+            ></el-image>
           </a>
         </el-aside>
       </el-container>
-        <div v-if="is_login" class="login">
-          <el-link :underline="false" icon="el-icon-edit">编辑</el-link>
-          <el-link :underline="false" icon="el-icon-delete">删除</el-link>
-        </div>
+      <div v-if="is_login" class="login">
+        <el-link :underline="false" @click="Modify">编辑</el-link>
+        <el-link :underline="false" @click="Delete(id)">删除</el-link>
+      </div>
     </el-card>
   </div>
 </template>
@@ -33,51 +41,67 @@ export default {
     return {};
   },
   methods: {
-    ClickToDetail: function(){
-        this.$router.push({
-          path: `/news/${this.id}`,
+    ClickToDetail: function () {
+      this.$router.push({
+        path: `/news/${this.id}`,
+      });
+    },
+    Modify: function () {
+      this.$router.push({
+        path: `/edit/${this.id}`,
+      });
+    },
+    Delete: function (new_id) {
+      this.$confirm("确认删除？")
+        .then((_) => {
+          let postData = {
+            token: this.$store.state.token,
+            id: new_id,
+          };
+          this.$axios.post("api/delete_news", postData);
         })
-    }
+        .catch((_) => {});
+    },
   },
-  mounted() {}
+  mounted() {},
 };
 </script>
 
 <style scoped>
-  .title{
-    font-size: 18px;
-    font-weight:bold;
-    font-family:"PingFang SC";
-  }
-  .summary {
-    font-size: 16px;
-  }
-  .date {
-    float: right;
-    padding: 3px 0;
-    color: gray;
-    font-size: 12px;
-  }
-  .image{
-    width: 100%;
-    height: 150px;
-    max-height: 150px;
-  }
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-  .box-card {
-    border-radius: 8px;
-  }
-  .el-aside {
-    width: 480px;
-  }
-  /*
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  font-family: "PingFang SC";
+}
+.summary {
+  font-size: 16px;
+}
+.date {
+  float: right;
+  padding: 3px 0;
+  color: gray;
+  font-size: 12px;
+}
+.image {
+  width: 100%;
+  height: 150px;
+  max-height: 150px;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+.box-card {
+  border-radius: 8px;
+}
+.el-aside {
+  width: 480px;
+}
+/*
   .el-main {
     color: #333;
   }
