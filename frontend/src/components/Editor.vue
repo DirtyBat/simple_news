ae96edd328655cd4d4a955211e78f58a2b10d2bb
DirtyBat <280051755@qq.com>
2023-04-09 20:42:43 +0800

    提交静态文件

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -35,12 +35,20 @@ import Vue from "vue";
 import "quill/dist/quill.core.css";
 import "quill/dist/quill.snow.css";
 import "quill/dist/quill.bubble.css";
-import Quill from 'quill';
-import { quillEditor } from "vue-quill-editor";
+// import Quill from 'quill';
+// import { quillEditor } from "vue-quill-editor";
+// import { ImageDrop } from "quill-image-drop-module";
+// import ImageResize from 'quill-image-resize-module';
+// // Quill.register('modules/imageResize', ImageResize);
+// Quill.register("modules/imageDrop", ImageDrop);
+
+import { quillEditor, Quill } from "vue-quill-editor";
 import { ImageDrop } from "quill-image-drop-module";
-import ImageResize from 'quill-image-resize-module';
-// Quill.register('modules/imageResize', ImageResize);
+import imageResize from 'quill-image-resize-module'
+Quill.register('modules/imageResize', imageResize)
 Quill.register("modules/imageDrop", ImageDrop);
+Vue.use(quillEditor);
+
 export default {
   name: "editor",
   components: { quillEditor },
